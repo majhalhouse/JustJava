d8e69597f0bda2356b4e6dff4a3296a3543e106b
Atinder Singh <majhalhouse@gmail.com>
2015-12-12 23:44:52 +0530

    completed email order task

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -33,12 +37,20 @@ public class MainActivity extends AppCompatActivity {
     public void increment(View view) {
 
         quantity = quantity + 1;
+        if (quantity > 100) {
+            quantity = 100;
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+        }
         display(quantity);
     }
 
     public void decrement(View view) {
 
         quantity = quantity - 1;
+        if (quantity < 1) {
+            quantity = 1;
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+        }
         display(quantity);
 
     }
@@ -64,8 +76,14 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + this.getName());
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        startActivity(Intent.createChooser(intent, "Send Email"));
     }
 
     /**
@@ -75,23 +93,42 @@ public class MainActivity extends AppCompatActivity {
      */
     private int calculatePrice(int quantity) {
         int price = quantity * 5;
+        if (hasWhippedCream()) {
+            price = price + (quantity);
+        }
+        if (hasChocolate()) {
+            price = price + (2 * quantity);
+        }
+        Log.v("Price :", String.valueOf(price));
         return price;
     }
 
     private String createOrderSummary(int pricePerOrder) {
-        String priceMessage = "Name= Kaptain Kunal";
+        String priceMessage = "Name=" + getName();
         priceMessage += "\nQuantity= " + quantity;
-        priceMessage += "\nAdd Whipped Cream= " + hasWhippedCream();
-        priceMessage += "\nTotal= " + (quantity * pricePerOrder);
+        priceMessage += "\nAdd Whipped Cream " + hasWhippedCream();
+        priceMessage += "\nAdd Chocolate " + hasChocolate();
+        priceMessage += "\nTotal= " + (calculatePrice(quantity));
         priceMessage += "\nThank you!";
         return priceMessage;
+
 
     }
 
     private Boolean hasWhippedCream()
     {
         CheckBox hasWhipppedCream=(CheckBox) findViewById(R.id.checkBox);
-        Boolean checkWhipped= hasWhipppedCream.isChecked();
-        return checkWhipped;
+        return hasWhipppedCream.isChecked();
+    }
+
+    private Boolean hasChocolate() {
+        CheckBox hasChocolate = (CheckBox) findViewById(R.id.checkBox_choc);
+        return hasChocolate.isChecked();
+    }
+
+    private String getName() {
+        EditText customerName = (EditText) findViewById(R.id.name_Editable);
+        //Log.i("test",customer);
+        return customerName.getText().toString();
     }
 }
